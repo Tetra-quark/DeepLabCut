Tetra-quark/DeepLabCut

commit 62b6ee1adbfa22bb932998d400c8eb4ea48d838c

Add TODOs. Improve legibility.

deeplabcut/pose_estimation_tensorflow/datasets/pose_deterministic.py
@@ -101,6 +101,7 @@ class DeterministicPoseDataset(BasePoseDataset):
         joints[:, 1] = image_width - joints[:, 1] - 1
         return joints
 
+    # TODO interesting this infact seems to swap symmetric joint IDs so we don't need to do it ourselves! If we use this.
     def mirror_joints(self, joints, symmetric_joints, image_width):
         # joint ids are 0 indexed
         res = np.copy(joints)
@@ -194,13 +195,14 @@ class DeterministicPoseDataset(BasePoseDataset):
 
         if self.has_gt:
             stride = self.cfg["stride"]
+
+            # TODO This is the only place where mirror joints is used.
             if mirror:
-                joints = [
-                    self.mirror_joints(
-                        person_joints, self.symmetric_joints, image.shape[1]
-                    )
-                    for person_joints in joints
-                ]
+                joints = []
+                for person_joints in joints:
+                    joints.append(self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]))
+                # joints = [self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]) for person_joints in joints]
+
             sm_size = np.ceil(scaled_img_size / (stride * 2)).astype(int) * 2
             scaled_joints = [person_joints[:, 1:3] * scale for person_joints in joints]
             joint_id = [person_joints[:, 0].astype(int) for person_joints in joints]

deeplabcut/pose_estimation_tensorflow/datasets/utils.py
@@ -25,6 +25,8 @@ def mirror_joints_map(all_joints, num_joints):
     res = np.arange(num_joints)
     symmetric_joints = [p for p in all_joints if len(p) == 2]
     for pair in symmetric_joints:
+        print(f"Res: {res}")
+        print(f"pair: {pair}")
         res[pair[0]] = pair[1]
         res[pair[1]] = pair[0]
     return res

deeplabcut/pose_estimation_tensorflow/predict_videos.py
@@ -522,6 +522,7 @@ def GetPoseS_GTF(cfg, dlc_cfg, sess, inputs, outputs, cap, nframes):
     if cfg["cropping"]:
         ny, nx = checkcropping(cfg, cap)
 
+    # TODO note to Jeff. How can we get the likelihood space for across the image dimensions? Is it accessible here
     pose_tensor = predict.extract_GPUprediction(
         outputs, dlc_cfg
     )  # extract_output_tensor(outputs, dlc_cfg)
@@ -549,11 +550,7 @@ def GetPoseS_GTF(cfg, dlc_cfg, sess, inputs, outputs, cap, nframes):
             )
             pose[:, [0, 1, 2]] = pose[:, [1, 0, 2]]
             # pose = predict.getpose(frame, dlc_cfg, sess, inputs, outputs)
-            PredictedData[
-                counter, :
-            ] = (
-                pose.flatten()
-            )  # NOTE: thereby cfg['all_joints_names'] should be same order as bodyparts!
+            PredictedData[counter, :] = (pose.flatten())  # NOTE: thereby cfg['all_joints_names'] should be same order as bodyparts!
         elif counter >= nframes:
             break
         counter += 1

commit d45e60b3359be05e39344ff28f8672f4fb9eb3aa

Fix bug introduced into augmentation mirroring in commit 62b6ee1adbf

deeplabcut/pose_estimation_tensorflow/datasets/pose_deterministic.py
@@ -207,10 +207,10 @@ class DeterministicPoseDataset(BasePoseDataset):
             # TODO This is the only place where mirror joints is used.
             # flips the coordinates of the labels and swaps the symmetric label names
             if mirror:
-                joints = []
-                for person_joints in joints:
-                    joints.append(self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]))
-                # joints = [self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]) for person_joints in joints]
+                # joints = [] # removing this should fix the bug I introduced.. must not delete the joints list.
+                # for person_joints in joints:
+                #     joints.append(self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]))
+                joints = [self.mirror_joints(person_joints, self.symmetric_joints, image.shape[1]) for person_joints in joints]
 
             sm_size = np.ceil(scaled_img_size / (stride * 2)).astype(int) * 2
             scaled_joints = [person_joints[:, 1:3] * scale for person_joints in joints]